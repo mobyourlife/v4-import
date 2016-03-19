a67739de9d8a1b745d5048b664ce0ee19f76fdc8
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-19 10:53:06 -0300

    Removing lib pattern as it's not necessary.

--- bin/import-old.js
+++ bin/import-old.js
@@ -1,13 +1,10 @@
 'use strict'
 
+// Load modules
 const api = require('mobv27-api/models')
 const mongoose = require('mongoose')
 
+// Connect to the database
 const defaultConnection = 'mongodb://localhost:27017/mob-v3'
-
-module.exports = ImportOld
-
-function ImportOld (connection) {
-  const database = connection || process.env.DATABASE || defaultConnection
-  mongoose.connect(database)
-}
+const database = process.env.DATABASE || defaultConnection
+mongoose.connect(database)
