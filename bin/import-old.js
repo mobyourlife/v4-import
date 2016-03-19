57db9f0b4a0328e2e58c8711bd760429678e392e
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-19 08:57:34 -0300

    Setting up import module.

--- bin/import-old.js
+++ bin/import-old.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const api = require('mobv27-api/models')
+const mongoose = require('mongoose')
+
+const defaultConnection = 'mongodb://localhost:27017/mob-v3'
+
+module.exports = ImportOld
+
+function ImportOld (connection) {
+  const database = connection || process.env.DATABASE || defaultConnection
+  mongoose.connect(database)
+}
