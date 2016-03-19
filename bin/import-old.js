0827fc824bb6821e96c823797e2b4f904c5b15e2
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-19 11:17:26 -0300

    Querying fanpages.

--- bin/import-old.js
+++ bin/import-old.js
@@ -3,3 +3,38 @@
 // Initialise import module
 const Importer = require('../lib/importer')
 const old = new Importer()
+
+/**
+ * Fetch data from the legacy database.
+ */
+function fetchData () {
+  let promise = new Promise((resolve, reject) => {
+    // Start jobs
+    let fanpages = old.getFanpages()
+
+    // Group jobs
+    let promises = []
+    promises.push(fanpages)
+
+    // Await for all promises
+    Promise.all(promises).then((data) => {
+      // Return a friendly object with everything
+      resolve({
+        fanpages: data[0]
+      })
+    }, reject)
+  })
+
+  return promise
+}
+
+/**
+ * Print data in console for debugging purposes.
+ * @param  {Object} data Imported data.
+ */
+function printData (data) {
+  console.log(data)
+}
+
+// Run the import process
+fetchData().then(printData)
