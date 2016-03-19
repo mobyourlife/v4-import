dae21b53d2e40c167b6d016f86b6466f8e783171
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-19 12:44:39 -0300

    Migrating users to the new schema.

--- bin/migrate.js
+++ bin/migrate.js
@@ -2,13 +2,14 @@
 
 // Load modules
 const Importer = require('../lib/importer')
+const migrate = require('../lib/migrate')
 
 /**
  * Migrate data from the legacy schema to the new schema.
  */
 function migrateData () {
   let legacy = new Importer()
-  legacy.fetchData().then(console.log)
+  legacy.fetchData().then(migrate)
 }
 
 // Execute data migration
