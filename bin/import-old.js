bacc245ebebada2e22b4e9b8595d98b783a87894
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-19 10:58:54 -0300

    Change module pattern a little bit.

--- bin/import-old.js
+++ bin/import-old.js
@@ -1,10 +1,5 @@
 'use strict'
 
-// Load modules
-const api = require('mobv27-api/models')
-const mongoose = require('mongoose')
-
-// Connect to the database
-const defaultConnection = 'mongodb://localhost:27017/mob-v3'
-const database = process.env.DATABASE || defaultConnection
-mongoose.connect(database)
+// Initialise import module
+const Importer = require('../lib/importer')
+const old = new Importer()
